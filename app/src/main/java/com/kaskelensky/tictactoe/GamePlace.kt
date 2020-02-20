72cb8959768709bbb41fe24c791652f3fcfbe0b6
Kaskelensky <180103157@stu.sdu.edu.kz>
2020-02-21 03:08:43 +0600

    Second

---
+++ app/src/main/java/com/kaskelensky/tictactoe/GamePlace.kt
@@ -0,0 +1,12 @@
+package com.kaskelensky.tictactoe
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class GamePlace : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_game_place)
+    }
+}
--- app/src/main/java/com/kaskelensky/tictactoe/MainActivity.kt
+++ app/src/main/java/com/kaskelensky/tictactoe/MainActivity.kt
@@ -1,12 +1,38 @@
 package com.kaskelensky.tictactoe
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageButton
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val easyBtn = findViewById<ImageButton>(R.id.easyBtn)
+        val normalBtn = findViewById<ImageButton>(R.id.normalBtn)
+        val hardBtn = findViewById<ImageButton>(R.id.hardBtn)
+        val wfBtn = findViewById<ImageButton>(R.id.wfBtn)
+
+        easyBtn?.setOnClickListener() {
+            startGame()
+        }
+        normalBtn?.setOnClickListener() {
+            startGame()
+        }
+        hardBtn?.setOnClickListener() {
+            startGame()
+        }
+        wfBtn?.setOnClickListener() {
+            startGame()
+        }
+    }
+
+    private fun startGame() {
+        //get id from API
+        val intent = Intent(this, GamePlace::class.java)
+        startActivity(intent)
     }
 }
